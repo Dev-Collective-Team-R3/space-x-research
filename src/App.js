3e499d370bc3a881e87a34032f144e612a62e6e1
Rahul Jain <rahulj93@gmail.com>
2021-02-22 22:55:52 -0500

    initiated the timeline component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Timeline from './components/timeline';
+
 function App() {
   return (
     <div className="container mx-auto">
       <h1 className="text-5xl font-bold text-green-500 p-5">
         Space X research
       </h1>
+      <Timeline></Timeline>
     </div>
   );
 }
---
+++ src/components/timeline.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+const Timeline = () => {
+  return (
+    <div> 
+      <h2>Space X Historical Timeline</h2> 
+    </div>
+  )
+}
+
+export default Timeline; 
