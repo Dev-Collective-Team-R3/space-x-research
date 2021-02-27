b35b2f217ac2bcfd8ed299b3ae0b9a552af1908c
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-02-27 02:13:51 -0500

    removed extra code from app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import LandingPage from "./components/LandingPage/LandingPage";
 function App() {
   return (
     <div className="container mx-auto">
-      <h1 className="text-5xl font-bold text-green-500 p-5">
-        <LandingPage />
-      </h1>
+      <LandingPage />
     </div>
   );
 }
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -4,7 +4,7 @@ import SpaceXVideo from '../../assets/spacex_med.mp4'
 const LandingPage = () => {
     return (
         <div>
-            <video autoPlay loop muted style={{ position: "fixed", width: "100vw", marginLeft: "auto", marginRight: "auto", padding: 0 }}>
+            <video autoPlay loop muted style={{ position: "fixed", width: "100vw", marginLeft: "auto", marginRight: "auto" }}>
                 <source src={SpaceXVideo} type="video/mp4" />
             </video>
         </div>
