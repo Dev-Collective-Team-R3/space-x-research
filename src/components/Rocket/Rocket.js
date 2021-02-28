5768010a54a3be796eec818d34e8a585178e5424
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-02-28 01:24:06 -0500

    showing multiple images on click

--- src/components/Rocket/Rocket.js
+++ src/components/Rocket/Rocket.js
@@ -8,6 +8,12 @@ const Rocket = () => {
 
     const [ rocket, setRocket ] = useState("")
 
+    const [ imageCounter, setImageCounter ] = useState(0)
+
+    const handleClick = () =>{
+        imageCounter+1 < rocket.flickr_images.length ? setImageCounter(imageCounter+1) : setImageCounter(0)
+    }
+
     useEffect(async () => {
         const data = await fetchSingleItem('rockets', rocketid)
         setRocket(data)
@@ -34,43 +40,43 @@ const Rocket = () => {
                         <table className="table-auto border border-black mt-5 ml-5">
                             <tbody>
                                 <tr>
-                                    <td className="border border-black">Stages</td>
-                                    <td className="border border-black">{rocket.stages}</td>
+                                    <td className="border border-black p-2">Stages</td>
+                                    <td className="border border-black p-2">{rocket.stages}</td>
                                 </tr>
                                 <tr>
-                                    <td className="border border-black">Boosters</td>
-                                    <td className="border border-black">{rocket.boosters}</td>
+                                    <td className="border border-black p-2">Boosters</td>
+                                    <td className="border border-black p-2">{rocket.boosters}</td>
                                 </tr>
                                 <tr>
-                                    <td className="border border-black">First Flight</td>
-                                    <td className="border border-black">{rocket.first_flight}</td>
+                                    <td className="border border-black p-2">First Flight</td>
+                                    <td className="border border-black p-2">{rocket.first_flight}</td>
                                 </tr>
                                 <tr>
-                                    <td className="border border-black">Height</td>
-                                    <td className="border border-black">{rocket.height.meters} meters</td>
+                                    <td className="border border-black p-2">Height</td>
+                                    <td className="border border-black p-2">{rocket.height.meters} meters</td>
                                 </tr>
                                 <tr>
-                                    <td className="border border-black">Diameter</td>
-                                    <td className="border border-black">{rocket.diameter.meters} meters</td>
+                                    <td className="border border-black p-2">Diameter</td>
+                                    <td className="border border-black p-2">{rocket.diameter.meters} meters</td>
                                 </tr>
                                 <tr>
-                                    <td className="border border-black">Mass</td>
-                                    <td className="border border-black">{rocket.mass.lb} lbs</td>
+                                    <td className="border border-black p-2">Mass</td>
+                                    <td className="border border-black p-2">{rocket.mass.lb} lbs</td>
                                 </tr>
                                 <tr>
-                                    <td className="border border-black" >Success Rate</td>
-                                    <td className="border border-black" >{rocket.success_rate_pct}</td>
+                                    <td className="border border-black p-2" >Success Rate</td>
+                                    <td className="border border-black p-2" >{rocket.success_rate_pct}</td>
                                 </tr>
                                 <tr>
-                                    <td className="border border-black" >Cost per launch</td>
-                                    <td className="border border-black" >{rocket.cost_per_launch/1000000} Million</td>
+                                    <td className="border border-black p-2" >Cost per launch</td>
+                                    <td className="border border-black p-2" >{rocket.cost_per_launch/1000000} Million</td>
                                 </tr>
                             </tbody>
                         </table>
 
                     </div>
-                    <div className="w-1/2 border">
-                        <img src={rocket.flickr_images[0]} alt="" className="object-cover h-full" />
+                    <div className="w-1/2 shadow-lg cursor-pointer" onClick={handleClick}>
+                        <img src={rocket.flickr_images[imageCounter]} alt="" className="object-cover h-full" />
                     </div>
                 </div>) : 
                 (<div>"Loading..."</div>)
