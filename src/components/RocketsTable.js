5f52527a58aeed6fdb66de07c49c95a3ba4cabb1
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-03-05 18:18:20 -0500

    moved table header to separate file

--- src/components/RocketsTable.js
+++ src/components/RocketsTable.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { rocketDataItems } from '../services/tableDataHeaders'
 
 const RocketsTable = ({ rockets }) => {
-
-    const rocketDataItems =[
-        {parameter: 'height', name: 'Height (meters)', accessor: 'height.meters'},
-        {parameter: 'diameter', name: 'Diameter (meters)', accessor: 'diameter.meters'},
-        {parameter: 'mass', name: 'Mass (lbs)', accessor: 'mass.lb'},
-        {parameter: 'fuel_amount', name: 'Fuel Amount (tons)', accessor: 'first_stage.fuel_amount_tons'},
-        {parameter: 'no_of_engines', name: 'Number of Engines', accessor: 'engines.number'},
-        {parameter: 'engine_type', name: 'Engine Type', accessor: 'engines.type'},
-        {parameter: 'primary_engine_prop', name: 'Primary Engine Propellant', accessor: 'engines.propellant_1'},
-        {parameter: 'secondary_engine_prop', name: 'Secondary Engine Propellant', accessor: 'engines.propellant_2'},
-        {parameter: 'landing_legs', name: 'Number of Landing Legs', accessor: 'landing_legs.number'},
-        {parameter: 'cost_launch', name: 'Cost per Launch (USD Millions)', accessor: 'cost_per_launch'},
-        {parameter: 'success_rate', name: 'Success Rate (%)', accessor: 'success_rate_pct'},
-        {parameter: 'first_flight', name: 'First Flight', accessor: 'first_flight'}
-    ]
 
     const dataGetter = (rocket, accessor) => {
         const keys = accessor.split('.');
@@ -32,7 +18,7 @@ const RocketsTable = ({ rockets }) => {
 
     const odd_table_row = 'bg-pink-200 py-1'
     const data_col_format = "text-left py-1"
-    const header_format = "text-left bg-pink-500 text-white"
+    const header_format = "text-left bg-pink-500 text-white py-2"
 
     return (
         <table className="w-full border-b-2 border-pink-500">
---
+++ src/services/tableDataHeaders.js
@@ -0,0 +1,14 @@
+export const rocketDataItems =[
+    {parameter: 'height', name: 'Height (meters)', accessor: 'height.meters'},
+    {parameter: 'diameter', name: 'Diameter (meters)', accessor: 'diameter.meters'},
+    {parameter: 'mass', name: 'Mass (lbs)', accessor: 'mass.lb'},
+    {parameter: 'fuel_amount', name: 'Fuel Amount (tons)', accessor: 'first_stage.fuel_amount_tons'},
+    {parameter: 'no_of_engines', name: 'Number of Engines', accessor: 'engines.number'},
+    {parameter: 'engine_type', name: 'Engine Type', accessor: 'engines.type'},
+    {parameter: 'primary_engine_prop', name: 'Primary Engine Propellant', accessor: 'engines.propellant_1'},
+    {parameter: 'secondary_engine_prop', name: 'Secondary Engine Propellant', accessor: 'engines.propellant_2'},
+    {parameter: 'landing_legs', name: 'Number of Landing Legs', accessor: 'landing_legs.number'},
+    {parameter: 'cost_launch', name: 'Cost per Launch (USD Millions)', accessor: 'cost_per_launch'},
+    {parameter: 'success_rate', name: 'Success Rate (%)', accessor: 'success_rate_pct'},
+    {parameter: 'first_flight', name: 'First Flight', accessor: 'first_flight'}
+]
